efc4331aa3488847233ada93ba5d210183be870c
下田　佳紀 <y.s@platinum.local>
2020-07-18 18:30:50 +0900

    デッキテストとデッキクラスの作成

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,8 +4,9 @@ require '../lib/card'
 
 class DeckTest < Minitest::Test
   def test_deck
-    Deck.new
-    puts @player_deck
-    puts @enemy_deck
+    player_deck = Deck.new(name: "ルビジの全カード",items: @current_cards)
+    enemy_deck = Deck.new(name: "信濃川の全カード",itmes: @current_cards)
+    assert_equal "ルビジの全カード", player_deck.name
+    assert_equal "信濃川の全カード", enemy_deck.name
   end
 end

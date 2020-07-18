86f969f93319e4935e11dcc85895ea186d30dea3
下田　佳紀 <y.s@platinum.local>
2020-07-18 18:56:27 +0900

    deckコード作成中

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,9 +4,11 @@ require '../lib/card'
 
 class DeckTest < Minitest::Test
   def test_deck
-    player_deck = Deck.new(name: "ルビジの全カード",items: @current_cards)
-    enemy_deck = Deck.new(name: "信濃川の全カード",itmes: @current_cards)
+    player_deck = Deck.new(name: "ルビジの全カード",player_cards: @player_cards)
+    enemy_deck = Deck.new(name: "信濃川の全カード",enemy_cards: @enemy_cards)
     assert_equal "ルビジの全カード", player_deck.name
     assert_equal "信濃川の全カード", enemy_deck.name
+    # puts player_deck.player_cards.current_card_name
+    # puts enemy_deck.enemy_cards.name.current_card_name
   end
 end

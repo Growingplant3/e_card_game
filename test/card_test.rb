e30b79e6521428c9ec7c38d988116b44e5cf52c8
下田　佳紀 <y.s@platinum.local>
2020-07-18 17:36:16 +0900

    カードテストとカードクラス作成

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,14 +3,14 @@ require '../lib/card.rb'
 
 class CardTest < Minitest::Test
   def test_card
-    emperor = Card.new(name: "皇帝",type: "emperor")
-    citizen = Card.new(name: "市民",type: "citizen")
-    slave = Card.new(name: "奴隷",type: "slave")
+    emperor = Card.new(name: "皇帝",type: 3)
+    citizen = Card.new(name: "市民",type: 2)
+    slave = Card.new(name: "奴隷",type: 1)
     assert_equal "皇帝", emperor.name
-    assert_equal "emperor", emperor.type
+    assert_equal 3, emperor.type
     assert_equal "市民", citizen.name
-    assert_equal "citizen", citizen.type
+    assert_equal 2, citizen.type
     assert_equal "奴隷", slave.name
-    assert_equal "slave", slave.type
+    assert_equal 1, slave.type
   end
 end

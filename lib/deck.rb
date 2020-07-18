2521865795736121962fa8ec1f794cd785c61f6c
下田　佳紀 <y.s@platinum.local>
2020-07-18 20:39:02 +0900

    デッキテストのコード修正

--- lib/deck.rb
+++ lib/deck.rb
@@ -11,11 +11,4 @@ class Deck
     @player_cards = [citizen,citizen,citizen,citizen,slave]
     @enemy_cards = [citizen,citizen,citizen,citizen,emperor]
   end
-
-  def show
-    @player_deck
-  end
-
-  @player_deck = Deck.new(name: "ルビジの全カード",player_cards: @player_cards)
-  @enemy_deck = Deck.new(name: "信濃川の全カード",enemy_cards: @enemy_cards)
 end

bda59dcbb00f62438fc3ab16d717901b0fe3610f
下田　佳紀 <y.s@platinum.local>
2020-07-20 17:51:36 +0900

    matchクラス関連を修正

--- test/match_test.rb
+++ test/match_test.rb
@@ -3,12 +3,12 @@ require '../lib/match'
 
 class MatchTest < Minitest::Test
   def test_match
-    @match = Match.new(turn: 5)
-    assert_equal 5, @match.turn
+    @match = Match.new(turn: 0)
+    assert_equal 0, @match.turn
   end
 
   def test_turn_pass
-    @match = Match.new(turn: 5)
-    assert_equal 4, @match.turn_pass
+    @match = Match.new(turn: 0)
+    assert_equal 1, @match.turn_pass
   end
 end

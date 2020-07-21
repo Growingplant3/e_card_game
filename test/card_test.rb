877384741c2aecca0748a1b271428621f66ab660
下田　佳紀 <y.s@platinum.local>
2020-07-21 21:27:24 +0900

    テストコード修正

--- test/card_test.rb
+++ test/card_test.rb
@@ -3,14 +3,9 @@ require '../lib/card.rb'
 
 class CardTest < Minitest::Test
   def test_card
-    emperor = Card.new(name: "皇帝",type: 3)
-    citizen = Card.new(name: "市民",type: 2)
-    slave = Card.new(name: "奴隷",type: 1)
+    emperor = Card.new(name: "皇帝")
+    citizen = Card.new(name: "市民")
     assert_equal "皇帝", emperor.name
-    assert_equal 3, emperor.type
     assert_equal "市民", citizen.name
-    assert_equal 2, citizen.type
-    assert_equal "奴隷", slave.name
-    assert_equal 1, slave.type
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -2,52 +2,33 @@ require 'minitest/autorun'
 require '../lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_player_deck
-    slave = Card.new(name: "奴隷", type: 1)
-    citizen = Card.new(name: "市民", type: 2)
-    @player_cards = [citizen,citizen,citizen,citizen,slave]
-    @player_deck = Deck.new(cards: @player_cards)
-    assert_equal "市民", @player_deck.cards.first.name
-    assert_equal 2, @player_deck.cards.first.type
-    assert_equal "奴隷", @player_deck.cards.last.name
-    assert_equal 1, @player_deck.cards.last.type
-    assert_equal 5, @player_deck.cards.length
-    assert_equal Array, @player_deck.cards.class
-  end
-
   def test_enemy_deck
-    emperor = Card.new(name: "皇帝", type: 3)
-    citizen = Card.new(name: "市民", type: 2)
+    emperor = Card.new(name: "皇帝")
+    citizen = Card.new(name: "市民")
     @enemy_cards = [citizen,citizen,citizen,citizen,emperor]
     @enemy_deck = Deck.new(cards: @enemy_cards)
     assert_equal "市民", @enemy_deck.cards.first.name
-    assert_equal 2, @enemy_deck.cards.first.type
     assert_equal "皇帝", @enemy_deck.cards.last.name
-    assert_equal 3, @enemy_deck.cards.last.type
     assert_equal 5, @enemy_deck.cards.length
     assert_equal Array, @enemy_deck.cards.class
   end
 
   def test_mix
-    emperor = Card.new(name: "皇帝", type: 3)
-    citizen = Card.new(name: "市民", type: 2)
+    emperor = Card.new(name: "皇帝")
+    citizen = Card.new(name: "市民")
     @enemy_cards = [citizen,citizen,citizen,citizen,emperor]
     @enemy_deck = Deck.new(cards: @enemy_cards)
     emperor_count = 0
     citizen_count = 0
-    slave_count = 0
     @enemy_deck.mix.each do |card|
       if card.name == "皇帝"
         emperor_count += 1
       elsif card.name == "市民"
         citizen_count += 1
-      elsif card.name == "奴隷"
-        slave_count += 1
       end
     end
     assert_equal 1, emperor_count
     assert_equal 4, citizen_count
-    assert_equal 0, slave_count
     assert_equal 5, @enemy_deck.mix.length
   end
 end
